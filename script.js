d0c121cc63b57a8b1810690b24dac4fd6dc715f3
welizaa <wmss6@aluno.ifal.edu.br>
2024-12-02 20:43:05 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+let num = parseFloat(prompt("Digite um numero:"));
+
+    if (num % 2 === 0) {
+      
+        alert('Voce digitou um numero par');
+    } else {
+
+        alert('Voce digitou um número impar');
+    }
